16cf0bb8672f53bfaa9eee2e6aff8b4540dd120e
fessfessor <fessfessor@gmail.com>
2019-06-17 14:12:26 +0300

    add aid kit and fall platform

---
+++ Assets/Sctipts/AidKit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AidKit : MonoBehaviour
+{
+    [SerializeField]
+    private int healthSize;
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.GetComponent<Health>() != null) {
+            col.gameObject.GetComponent<Health>().health += healthSize;
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
---
+++ Assets/Sctipts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    Vector3 localScale;
+
+    void Start()
+    {
+        localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        localScale.x = gameObject.GetComponentInParent<Health>().health * 0.01f;
+        transform.localScale = localScale;
+    }
+}
