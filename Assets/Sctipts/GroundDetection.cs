6ece1bc2cf79e638a99e6f21cbc96c3464afd56c
fessfessor <fessfessor@gmail.com>
2019-06-27 23:27:07 +0300

    make spikes

--- Assets/Sctipts/GroundDetection.cs
+++ Assets/Sctipts/GroundDetection.cs
@@ -7,6 +7,7 @@ public class GroundDetection : MonoBehaviour
     public bool isGrounded;
 
     private void OnCollisionEnter2D(Collision2D col) {
+       // Debug.Log("tag - " + col.gameObject.transform.parent.transform.tag);
         
         if (col.gameObject.transform.parent.transform.name == "Ground" || col.gameObject.transform.parent.transform.CompareTag("Ground")) {
             isGrounded = true;
@@ -14,7 +15,7 @@ public class GroundDetection : MonoBehaviour
     }
 
     private void OnCollisionExit2D(Collision2D col) {
-        if (col.gameObject.transform.parent.transform.name == "Ground" || col.gameObject.transform.parent.CompareTag("Ground")) {
+        if (col.gameObject.transform.parent.transform.name == "Ground" || col.gameObject.transform.parent.transform.CompareTag("Ground")) {
             isGrounded = false;
         }
     }
---
+++ Assets/Sctipts/Spikes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+
+    [SerializeField] int damage;
+
+    private void OnCollisionEnter2D(Collision2D col) {
+
+        //Debug.Log(col.gameObject.transform.name);
+        if(col.gameObject.transform.name == "Player") {
+            // получаем здоровье игрока
+            var health = GameManager.Instance.healthContainer[col.gameObject];
+            health.takeHit(damage);
+
+            PlatformerTools.ShowHealthBar(col.gameObject);
+
+
+        }
+    }
+}
