5d07da795bc78fec5c57d6e8b689a7de3567aac7
fessfessor <fessfessor@gmail.com>
2019-11-13 00:47:30 +0300

    cool lamp

---
+++ Assets/Sctipts/Effects/LampMotion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampMotion : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Rigidbody2D playerRb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        playerRb = GameManager.Instance.player.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if(col.gameObject == GameManager.Instance.player) {
+            rb.velocity = playerRb.velocity/2;
+        }
+    }
+}
